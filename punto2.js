991c61ba08bcb5e0c1a3b8af9bbc7fce02495e69
Jcamilo8aR <camilo8ar23@gmail.com>
2023-09-04 18:25:11 -0500

    Continuacion Taller

--- punto2.js
+++ punto2.js
@@ -1,8 +1,9 @@
 function obtenerNombre(codigo){
     return codigo.split(":")[1]
 }
+
+let obtenerNombre1=(codigo)=> codigo(":")[1]
 console.log(obtenerNombre("ARQ255:Sara Bel-Sun"))
 
-// EN FLECHA
 let obtenerNombre2=(codigo)=> codigo(":")[1]
 console.log(obtenerNombre("ARQ2556:Nodin Chavdri"))
---
+++ punto4.js
@@ -0,0 +1,32 @@
+let sables = [3,7,-2,9,-4,-3,5,10,4,1]
+let defectuosos = 0
+
+/* let mapa=sables.map(function(sable){
+    mapa.forEach(function(sable){
+        if (sables < 0){
+            defectuosos = defectuosos +1
+        }
+    })
+    return defectuosos
+}) */
+
+/* sables.foreach(function(sable){
+    if (sables < 0){
+        defectuosos = defectuosos +1
+    }
+    return sable
+})
+
+console.log(sables); */
+
+
+
+function analizarPlanetas(sables){
+    // Recorrer un arreglo
+    let mapa=sables.map(function(sable){
+        return (sables)
+    })   
+    return mapa
+}
+
+analizarPlanetas()
